f58eeab65657398c04a9f23a6fb9cf6db2cf0155
Juan Carlos Londoño <juan.londono11@udea.edu.co>
2020-11-26 13:22:05 -0500

    Car list included

--- src/components/vehicle/CardVehicle.js
+++ src/components/vehicle/CardVehicle.js
@@ -9,21 +9,24 @@ export default class CardVehicle extends Component {
     }
 
     render() {
+        console.log(this.props.data);
         return (
-            <div align='center'>
-                <Card style={{ width: '20rem' }}>
-                    <Card.Header> PLACA: </Card.Header>
-                    <ListGroup variant="flush">
-                        <ListGroup.Item>Propietario: </ListGroup.Item>
-                        <ListGroup.Item>Color: </ListGroup.Item>
-                        <ListGroup.Item>Marca: </ListGroup.Item>
-                        <ListGroup.Item>
-                            Historial: 
-                            <Card.Link href=''> ver mas</Card.Link>
-                        </ListGroup.Item>
-                    </ListGroup>
-                </Card>
-            </div>
+            <Card style={{ margin: 10, flex:1, width: '20rem' }}>
+                <Card.Header> PLACA: {this.props.data.placa}</Card.Header>
+                <ListGroup variant="flush">
+                    <ListGroup.Item>Propietario: 
+                        <Card.Link href=''> {this.props.data.propietario.nombre}</Card.Link>
+                    </ListGroup.Item>
+                    <ListGroup.Item>Color: {this.props.data.Color}</ListGroup.Item>
+                    <ListGroup.Item>Marca: {this.props.data.marca}</ListGroup.Item>
+                    <ListGroup.Item>Tipo: {this.props.data.tipo}</ListGroup.Item>
+                    <ListGroup.Item>Modelo: {this.props.data.modelo}</ListGroup.Item>
+                    <ListGroup.Item>
+                        Historial: 
+                        <Card.Link href=''> ver mas</Card.Link>
+                    </ListGroup.Item>
+                </ListGroup>
+            </Card>
         );
     }
 }
--- src/components/vehicle/ListVehicle.js
+++ src/components/vehicle/ListVehicle.js
@@ -1,19 +1,52 @@
 import React, { Component } from 'react';
 import CardVehicle from './CardVehicle';
+import firebase from 'firebase';
+import { CardColumns } from 'react-bootstrap';
 
 export default class ListVehicle extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            cars: [],
+            update: false
         }
     }
 
+    async componentDidMount(){
+        await this.getCarList();
+    }
+
+    getCarList(){
+        this.setState({
+            update:true
+        });
+        firebase.auth().currentUser.getIdToken(true).then(function(idToken) {
+            const url = 'https://run.mocky.io/v3/513fe8c4-4e1a-42d4-927e-14857a88b8b3';
+            fetch(url, {
+                method: 'GET',
+                headers: new Headers({
+                    'Content-type': 'application/json',
+                    'idToken': idToken,
+                }),
+            }).then((Response) => Response.json())
+            .then((ResponseJson) => {
+                this.setState({
+                    update:false,
+                    cars: ResponseJson
+                })
+            });
+        }.bind(this))
+    }
+
+
+
     render() {
         return (
-            <div>
-                <CardVehicle/>
+            <div style={{display: 'flex', flexDirection: 'row'}}>
+                {this.state.cars.map(car => 
+                    <CardVehicle data = {car} />
+                )}
             </div>
-            
         );
     }
 }
